158758852c8a2db8b1d19f6a9e4641578a2a131c
Neil <neilpinef@gmail.com>
2024-09-12 17:58:37 +0530

    Update 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './index.css';
-import Navbar from './Navbar';
+import NavBar from './NavBar';
 import FrontPage from './FrontPage';
 import Discover from './discover';
 import Profile from './profile';
@@ -14,16 +14,17 @@ import JobList from './joblist';
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<FrontPage />} />
-        <Route path="/discover" element={<Discover />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/hiring" element={<Hiring />} />
-        <Route path="/joblist" element={<JobList />} />
-      </Routes>
+      <NavBar />
+        <Routes>
+          <Route path="/" element={<FrontPage />} />
+          <Route path="/discover" element={<Discover />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/hiring" element={<Hiring />} />
+          <Route path="/joblist" element={<JobList />} />
+        </Routes>
     </div>
   );
 }
+
 
 export default App;
